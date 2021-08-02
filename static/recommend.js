d85e41ac0cd1fd066e9c0bebe2894d057f8cdb20
Rahul Waghmare <rwaghmare981@gmail.com>
2021-08-02 14:54:53 +0530

    Update recommend.js

--- static/recommend.js
+++ static/recommend.js
@@ -12,7 +12,7 @@ $(function() {
   source.addEventListener('input', inputHandler);
 
   $('.movie-button').on('click',function(){
-    var my_api_key = '983e7ca75021f7d73509b1b4135dc622';
+    var my_api_key = 'YOUR API KEY';
     var title = $('.movie').val();
     if (title=="") {
       $('.results').css('display','none');
@@ -26,7 +26,7 @@ $(function() {
 
 // will be invoked when clicking on the recommended movies
 function recommendcard(e){
-  var my_api_key = '983e7ca75021f7d73509b1b4135dc622';
+  var my_api_key = 'YOUR API KEY';
   var title = e.getAttribute('title'); 
   load_details(my_api_key,title);
 }
